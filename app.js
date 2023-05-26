331f1051a0d6f2c4769127658cb2b2527f8b1f89
AndresLince <lynxdarmatron@gmail.com>
2023-05-26 16:22:37 -0500

    Clase 3 package.json and npm

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const { frutas, dinero } = require('./frutas');
+const cowsay = require("cowsay");
 
 frutas.forEach(item => {
     console.count(item);
 });
+
+console.log(cowsay.say({
+    text: "Hola amigos de bluuweb",
+    e: "oO",
+    T: "U "
+}));
 
 console.log(dinero);
