c9de8cbfb1f370571208d26ae601bb9a2d0df549
AndresLince <lynxdarmatron@gmail.com>
2023-06-07 16:53:41 -0500

    Clase 5

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     res.send('Mi respuesta desde express v.2')
 });
 
 app.get('/service', (req, res) => {
     res.send('Estas en la pagina de servicios')
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(__dirname + '/public/404.html')
 });
 
 app.listen(port, () => {
