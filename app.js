fb5070838307caf87d5a1b4cf36bcf0b6d304d12
AndresLince <lynxdarmatron@gmail.com>
2023-06-14 06:04:12 -0500

    Add mongoose

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const user = '';
+const password = '';
+const uri = '';
+
+mongoose.connect(uri, {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log(e));
+
 const app = express();
 const port = process.env.port || 3000;
 
