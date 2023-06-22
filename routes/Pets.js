297176aff6a5f2c48b405a776631ca7b885f2431
AndresLince <lynxdarmatron@gmail.com>
2023-06-22 16:23:02 -0500

    Add pet details route

--- routes/Pets.js
+++ routes/Pets.js
@@ -29,6 +29,22 @@ router.post('/', async(req, res) => {
     } catch (error) {
         console.log(error);
     }
-})
+});
+
+router.get('/:id', async(req, res) => {
+    const id = req.params.id;
+
+    try {
+        const petDb = await pet.findOne({ _id: id });
+        res.render('detail', {
+            pet: petDb,
+            titulo: 'Detalle de mascota'
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/404');
+    }
+
+});
 
 module.exports = router;
